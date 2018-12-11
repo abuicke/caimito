e6f9d942bc24944edf9c0743431e06a4f74ac52d
abuicke <mosesjayjayjay@gmail.com>
2018-12-11 13:32:55 +0000

    added string utils

---
+++ caimito/src/main/java/ie/moses/caimito/text/StringUtils.java
@@ -0,0 +1,19 @@
+package ie.moses.caimito.text;
+
+public final class StringUtils {
+
+    public static String toCsv(Iterable<CharSequence> strings) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (CharSequence string : strings) {
+            stringBuilder.append(string)
+                    .append(',');
+        }
+        int lastCharIndex = stringBuilder.length() - 1;
+        stringBuilder.deleteCharAt(lastCharIndex);
+        return stringBuilder.toString();
+    }
+
+    private StringUtils() {
+    }
+
+}
