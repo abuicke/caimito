c05012288844dfab12cb4c8eae3ce18ed3777570
abuicke <buickea@gmail.com>
2024-03-25 15:58:23 +0000

    caimito publishing to maven (but without source)

--- caimito/build.gradle.kts
+++ caimito/build.gradle.kts
@@ -1,8 +1,6 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
-    `maven-publish`
-    signing
 }
 
 android {
@@ -80,3 +78,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
+
+apply(from = "../publish-package.gradle")
